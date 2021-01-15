ff5c4932af010110c16f91281e8afb85b3e03404
WhissManz97 <77012403+WhissManz97@users.noreply.github.com>
2021-01-15 11:41:59 -0600

    Commit Inicial, ComboControls
    Commit Inicial, Combo Controls

---
+++ SeleniumCSharp/CostumControl.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharp
+{
+    public class CostumControl : DriverHelper
+    {
+        public static void ComboBox(String controlName, string value) {
+
+         IWebElement comboControl = Driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+
+        comboControl.Clear();
+        comboControl.SendKeys(value);
+
+        Driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text()='{value}']")).Click();
+        }
+    }
+}
+
+
---
+++ SeleniumCSharp/UnitTest1.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumCSharp
+{
+    public class Tests : DriverHelper
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+            Driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demo.aspnetawesome.com/");
+
+            Driver.FindElement(By.Id("Meal")).SendKeys("Tomato");
+            Driver.FindElement(By.XPath("/html/body/div[2]/div/div[3]/main/div[1]/div[4]/div[2]/div[2]/div/ul/li[1]/label/div[1]/div/div")).Click();
+
+           CostumControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almonds");
+
+            Console.WriteLine("Test1");
+            Assert.Pass();
+        }
+    }
+}
+
